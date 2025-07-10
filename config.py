73d272672c12ff35f8bd37102217abf7d37ca8e0
ixamit007 <radhikaxmusicbot@gmail.com>
2025-07-11 02:09:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/astamusicbot/VILLAIN_MUSIC",
+    "https://github.com/ixamit007_astaamit",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
